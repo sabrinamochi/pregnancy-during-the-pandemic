5983a8d5ddfec354eb8a81bb1c2890148d18f52f
szuyu <szuyuchen0101@gmail.com>
2021-04-23 01:35:17 -0400

    d3

--- scripts/html.js
+++ scripts/html.js
@@ -45,7 +45,7 @@ function loadData() {
   }, {});
 
   data.timestamp = Date.now();
-  data.basepath = ENV === 'dev' ? '' : 'https://pudding.cool';
+  data.basepath = ENV === 'dev' ? '' : 'https://pudding.cool/';
   return Promise.resolve(data);
 }
 
