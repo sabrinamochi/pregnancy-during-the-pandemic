688cf2c8851196fffd657436c19d5d6e93cc6ec8
szuyu <szuyuchen0101@gmail.com>
2021-04-23 01:21:42 -0400

    new layout

--- src/js/intro.js
+++ src/js/intro.js
@@ -9,69 +9,65 @@ const $insecurity = d3.selectAll('.insecurity-button')
 const $inequality = d3.selectAll('.inequality-button')
 
 
-let w = window.innerWidth;
-let isMobile = w <= 768 ? true : false;
+let w = window.innerWidth,
+    isMobile = w <= 768 ? true : false,
+    videoVisible,
+    buttonClicked = false;
 
 function stopVideo() {
     $videoContainer.style('display', 'none')
+    videoVisible = false;
     $video.pause()
     startOverview()
 }
 
 function playVideo() {
+    buttonClicked = true;
     $openingContainer.style('display', 'none')
     $videoContainer.style('display', 'block')
-    $video.play()
+    videoVisible = true;
+    $video.addEventListener('ended', stopVideo)
+    resize()
 }
 
-let count = 0
-
 function resize() {
-    if ($video) {
+    if (videoVisible && buttonClicked) {
         w = window.innerWidth;
         isMobile = w <= 768 ? true : false;
-        if (count == 0) {
-            if (isMobile) {
-                $video.setAttribute("src", "assets/images/opening-video-mobile.mp4");
-            } else {
-                $video.setAttribute("src", "assets/images/opening-video.mp4");
-            }
-            count++
-        } else if (count > 0) {
-            $video.pause()
-            if (isMobile) {
-                $video.setAttribute("src", "assets/images/opening-video-mobile.mp4");
-            } else {
-                $video.setAttribute("src", "assets/images/opening-video.mp4");
-            }
-            $video.load()
-            $video.play()
-        } else if (count < 0) {
-            $video.pause()
+        $video.pause()
+        if (isMobile) {
+            $video.setAttribute("src", "assets/images/opening-video-mobile.mp4");
+        } else {
+            $video.setAttribute("src", "assets/images/opening-video.mp4");
         }
+        $video.load()
+        $video.play()
     }
 
 }
 
 function startOverview() {
-    count = -1
-    d3.selectAll('.overview').style('display', 'flex')
+    if (!videoVisible){
+        let i = 0
+        d3.selectAll('.overview').style('display', 'flex')
+        document.querySelector('#overview-infection-button')
+            .addEventListener("animationend", function() {
+                i = i + 1
+                if (i == 3) {
+                    startTab('infection');
+                }
+            }, false);
+    }   
 }
 
 function startTab(tabName) {
-    d3.selectAll('.tab button').style('color', '#000')
-    d3.selectAll(`.${tabName}-button`).style('color', '#E8356D')
     location.href = `${tabName}.html`;
 }
 
-function init() {
-    if ($video) {
-        resize()
-        $startButton.on('click', playVideo)
-        $skipButton.on('click', stopVideo)
-        $video.addEventListener('ended', stopVideo)
-    }
 
+function init() {
+    $startButton.on('click', playVideo)
+    $skipButton.on('click', stopVideo)
     $infection.on('click', () => startTab('infection'))
     $isolation.on('click', () => startTab('isolation'))
     $insecurity.on('click', () => startTab('insecurity'))
